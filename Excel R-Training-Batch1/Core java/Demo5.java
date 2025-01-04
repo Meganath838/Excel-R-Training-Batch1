b0851de867e0a67e7860d76f71e75b7b4ac9f863
Meganath838 <mokalameganath@gmail.com>
2025-01-04 12:24:08 +0530

    Add files via upload
    Demo5

---
+++ Excel R-Training-Batch1/Core java/Demo5.java
@@ -0,0 +1,12 @@
+public class Demo5{
+    public static void main (String args[]) {
+        String name = "name";
+        String pwd = "place";
+        while((name == "name") && (pwd == "place")) {
+            System.out.println("hello user happy new year");
+            
+        }
+        
+
+    }
+}
