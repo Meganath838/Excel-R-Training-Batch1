45c51e679914de796e01b6935d9ebc7ab89ad18b
Meganath838 <mokalameganath@gmail.com>
2025-01-23 21:53:42 +0530

    checking username and password2

---
+++ Demo007.java
@@ -0,0 +1,15 @@
+public class Demo007 {
+
+        @SuppressWarnings("StringEquality")
+        public static void main(String[] args) {
+    
+            String Username = "Mani";
+            String Password = "Mani417";
+    
+            while((Username == "Mani") && (Password == "Mani417")) {
+                System.out.println("Welcome to Mallareddy university");
+                break;
+            }
+        }
+     }
+
