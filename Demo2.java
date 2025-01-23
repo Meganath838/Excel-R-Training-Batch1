c5be57edd6f83c7bd16af897aaeb7e5ce885997c
Meganath838 <mokalameganath@gmail.com>
2025-01-23 21:34:11 +0530

    greatest of 2 numbers

---
+++ Demo2.java
@@ -0,0 +1,11 @@
+
+class Demo2 {
+    public static void main(String[] args) {
+        int time = 78;
+if (time < 18) {
+  System.out.println("Good day.");
+} else {
+  System.out.println("Good evening.");
+}
+    }
+}
