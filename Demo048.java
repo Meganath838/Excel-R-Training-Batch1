9c8afde71ffe5dc672ea88567eb14da4b1fab2ac
Meganath838 <mokalameganath@gmail.com>
2025-01-31 11:13:55 +0530

    sum of digits

---
+++ Demo048.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Demo048 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+
+        
+        int sum = 0;
+
+        
+        if (number < 0) {
+            number = -number;
+        }
+
+        
+        while (number > 0) {
+            sum += number % 10;  
+            number /= 10;     
+        }
+
+        System.out.println("Sum of digits: " + sum);
+
+        scanner.close();
+    }
+}
